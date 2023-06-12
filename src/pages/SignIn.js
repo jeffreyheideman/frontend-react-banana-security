c67d20b2ac5dba3cfc0f55148a05e5acbf6b2698
jeffreyheideman <jeffrey_heideman@hotmail.com>
2023-06-12 11:28:20 +0200

    De basis opdrachten afgerond

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useContext} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
 
 function SignIn() {
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate()
+
+    function handleFormSubmit(e) {
+        e.preventDefault()
+        login()
+        navigate("/profile")
+
+    }
+
   return (
     <>
       <h1>Inloggen</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
 
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <p>*invoervelden*</p>
-        <button>Inloggen</button>
+        <button type="submit">Inloggen</button>
       </form>
 
       <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
